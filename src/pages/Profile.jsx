65f706a9db4dc83fac2b278e4651244175994b42
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-05-10 13:06:02 +0700

    kapital

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { useNavigate, Link } from 'react-router-dom'
 const Profile = () => {
   return(
     <section>
-      <Link to={`/todolist`}>todolist</Link>
+      <Link to={`/todolist`}>Todolist</Link>
     </section>
   )
 }
